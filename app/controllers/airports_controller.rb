e1409100375ffd8231022fea21c1d2889bddc936
subu8098 <subuvarathan8098@gmail.com>
2025-08-13 14:45:38 +0530

    implemented the dynamic page from json file without datalist and with dynamic content

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,4 +1,8 @@
 class AirportsController < ApplicationController
   def show
+  airport_key = params[:key] # e.g. "ipoh-airport"
+  json_path = Rails.root.join("public", "airport-routes.json")
+  airports_json = JSON.parse(File.read(json_path))
+  @airport_data = airports_json[airport_key]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get "airports/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "airports/show"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get '/ipoh-airport', to: 'airports#show'
+  get '/airports/:key', to: 'airports#show', as: :airport
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
   # root "posts#index"
 end
-
